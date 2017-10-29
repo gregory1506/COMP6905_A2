7fd2363f232c5c61fa05ce26b5eb4133c2fe4b12
gregory1506 <gjotomcat@gmail.com>
2017-10-29 18:41:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def about():
 if __name__ == '__main__':
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
     app.run(HOST, PORT)
